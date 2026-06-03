4c0127fa71af5924855cd97516a613000605d7f4
chriscle2013 <chriscle2013@gmail.com>
2026-06-03 01:47:57 -0500

    Reorganize getters and setters in Report.java

--- src/main/java/com/callerIdApplication/entity/Report.java
+++ src/main/java/com/callerIdApplication/entity/Report.java
@@ -24,19 +24,17 @@ public class Report {
         this.spammer = true;
     }
     
-    // Getters y Setters
+    // Getters
     public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    
     public String getPhoneNumber() { return phoneNumber; }
-    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
-    
     public String getCategory() { return category; }
-    public void setCategory(String category) { this.category = category; }
-    
     public String getComment() { return comment; }
-    public void setComment(String comment) { this.comment = comment; }
-    
     public boolean isSpammer() { return spammer; }
+    
+    // Setters
+    public void setId(Long id) { this.id = id; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public void setCategory(String category) { this.category = category; }
+    public void setComment(String comment) { this.comment = comment; }
     public void setSpammer(boolean spammer) { this.spammer = spammer; }
 }
