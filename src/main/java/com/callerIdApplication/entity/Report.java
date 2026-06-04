f2b9a5d94269540c7e12a99b33935845118bd765
chriscle2013 <chriscle2013@gmail.com>
2026-06-03 19:57:05 -0500

    Update Report.java

--- src/main/java/com/callerIdApplication/entity/Report.java
+++ src/main/java/com/callerIdApplication/entity/Report.java
@@ -10,9 +10,7 @@ public class Report {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @Column(name = "phone_number")
     private String phoneNumber;
-    
     private String category;
     private String comment;
     private boolean spammer;
@@ -26,17 +24,19 @@ public class Report {
         this.spammer = true;
     }
     
-    // Getters
+    // Getters y Setters
     public Long getId() { return id; }
-    public String getPhoneNumber() { return phoneNumber; }
-    public String getCategory() { return category; }
-    public String getComment() { return comment; }
-    public boolean isSpammer() { return spammer; }
-    
-    // Setters
     public void setId(Long id) { this.id = id; }
+    
+    public String getPhoneNumber() { return phoneNumber; }
     public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    
+    public String getCategory() { return category; }
     public void setCategory(String category) { this.category = category; }
+    
+    public String getComment() { return comment; }
     public void setComment(String comment) { this.comment = comment; }
+    
+    public boolean isSpammer() { return spammer; }
     public void setSpammer(boolean spammer) { this.spammer = spammer; }
 }
