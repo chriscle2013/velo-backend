cfaf103bf2753e4ab8ddb487e753864867fe4ae2
chriscle2013 <chriscle2013@gmail.com>
2026-06-27 12:28:10 -0500

    Update SmsDao.java

--- src/main/java/com/callerIdApplication/repostitory/SmsDao.java
+++ src/main/java/com/callerIdApplication/repostitory/SmsDao.java
@@ -1,7 +1,11 @@
 package com.callerIdApplication.repostitory;
+
 import com.callerIdApplication.entity.SmsReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface SmsDao extends JpaRepository<SmsReport, Long> { }
+public interface SmsDao extends JpaRepository<SmsReport, Long> {
+    List<SmsReport> findByPhoneNumber(String phoneNumber);
+}
